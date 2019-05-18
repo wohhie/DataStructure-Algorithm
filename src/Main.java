912c49e07542f47a5b9ca7bcd8ef188a1599973f
wohhie <wohhie92@gmail.com>
2019-05-18 15:09:23 -0230

    Insertion Sort Updated.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Data Structure & Algorithm!");
+
+        // Insertion Sort
+        InsertionSort insertionSort = new InsertionSort();
+        insertionSort.execute();
+    }
+}
