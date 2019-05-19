bcbbd36aa0ec9196e8c237c1243fcaba5f578f71
wohhie <wohhie92@gmail.com>
2019-05-18 22:00:40 -0230

    Insertion Sort Updated.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 
+
+
     public static void main(String[] args){
         System.out.println("Data Structure & Algorithm!");
 
@@ -10,5 +12,7 @@ public class Main {
 
         MergeSort mergeSort = new MergeSort();
         mergeSort.execute();
+
+
     }
 }
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,48 +1,103 @@
 public class MergeSort {
 
 
-    private void mergeSort(int arr[], int l, int m, int r){
+
+    private void mergeSort(int[] arr) {
+        // if empty array
+        if (arr == null){
+            return;
+        }
+
+        if (arr.length > 1){
+            // finding the mid point
+            int mid = (arr.length) / 2;
+            //System.out.print(mid);
+
+            // split the left
+            System.out.println();
+            System.out.print("Left Split: ");
+            int[] left = new int[mid];
+            for (int i = 0; i < mid; i++){
+                left[i] = arr[i];
+                System.out.print(left[i] + " ");
+            }
+            System.out.println();
+
+
+            System.out.print("Right Array: ");
+            // split the right
+            int[] right = new int[arr.length - mid];
+            for (int i = mid; i < arr.length; i++){
+                right[i - mid] = arr[i];
+                System.out.print(right[i - mid] + " ");
+            }
+            System.out.println();
+
+
+            mergeSort(left);
+            mergeSort(right);
+
+
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            // merge left and right arrays
+            while (i < left.length && j < right.length){
+                if (left[i] < right[j]){
+                    arr[k] = left[i];
+                    i++;
+                }else{
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < left.length){
+                arr[k] = left[i];
+                i++;
+                k++;
+            }
+
+            while (j < right.length){
+                arr[k] = right[j];
+                j++;
+                k++;
+            }
+
+        }
 
     }
 
-    /**
-     *
-     * @param arr   array
-     * @param low   first index
-     * @param high  length
-     */
-    private void sort(int[] arr, int low, int high) {
-        if (low < high){
-            // Find the middle point
-            int mid = (low + high) / 2;
-            System.out.println(mid);
-
-            // sort the first and second halves
-            sort(arr, low, mid);
-            sort(arr, mid + 1, high);
-
-            // Merge the sorted haves
-            mergeSort(arr, low, mid, high);
+    public void printArray(int[] array){
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
         }
-    }
-
-    private void print(int arr[]){
-        int n = arr.length;
-        for (int i = 0; i < n; i ++){
-            System.out.print(arr[i] + " ");
-        }
-
-        System.out.println();
     }
 
 
     public void execute(){
-        int arr[] = {12, 11, 13, 5, 6, 7};
         // System.out.println(arr.length);
         // Array will be printed.
         //print(arr);
 
-        sort(arr, 0, arr.length - 1);
+        int arr[] = {12, 11, 13, 5, 6, 7};
+
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+
+        mergeSort(arr);
+
+
+        System.out.println("\n");
+        System.out.println("Sorted array is");
+
+        for(int i=0; i<arr.length; i++)
+            System.out.print(arr[i]+" ");
     }
 
 }
