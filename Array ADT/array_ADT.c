ac0166574721250ed6a7f40ee481884c4ea87386
wohhie <wohhie92@gmail.com>
2020-11-02 22:27:09 -0330

    Array ADT: display added

--- Array ADT/array_ADT.c
+++ Array ADT/array_ADT.c
@@ -7,8 +7,18 @@ struct Array {
     int length;
 };
 
+
+void Display(Struct Array arr){
+    int i;
+    printf("Elements are:\n");
+    for(i = 0; i < arr.length; i++)
+        printf("%d ", arr.A[i])
+}
+
+
 int main() {
     struct Array arr;
+    int n, i;
     printf("Enter the size of an array: ");
     scanf("%d", &arr.size);
 
@@ -16,6 +26,17 @@ int main() {
     arr.A = (int *) malloc(arr.size * sizeof(int));
     arr.length = 0;
 
+
+    // getting the values
+    printf("Enter number of numbers");
+    scanf("%d", &n);
+
+    printf("Enter all elements:\n")
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr.A[i]);
+
+
+    arr.length = n;
 
     return 0;
 }
