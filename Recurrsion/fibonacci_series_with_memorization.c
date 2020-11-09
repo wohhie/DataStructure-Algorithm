269f251db470c9c77bce9ceaa641ff1e215dac59
wohhie <wohhie92@gmail.com>
2020-11-08 21:46:03 -0330

    update fibonacci series with memorization

--- Recurrsion/fibonacci_series_with_memorization.c
+++ Recurrsion/fibonacci_series_with_memorization.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
+int F[10];
+
+int fibonacci(int n){
+    if (n <= 1){
+        F[n] = n;
+        return n;
+    }else{
+        if (F[n-2] == -1){
+            F[n-2] = fibonacci(n-2);
+        }
+
+        if (F[n-1] == -1){
+            F[n-1] == fibonacci(n-1);
+        }
+
+        return F[n-2] + F[n-1];
+    }
+}
 
 
 int main() {
+
+    int i;
+    for(i = 0; i < 10; i++){
+        F[i] = -1;
+    }
+
     printf("Fibonacci Series with Memorization: \n");
-    printf("%d \n", fibonacci(6));
+    printf("%d \n", fibonacci(5));
 
     return 0;
 }
