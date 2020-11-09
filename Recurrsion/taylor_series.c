0ed6bdc0264b9438784f05814e242d168c47e61e
wohhie <wohhie92@gmail.com>
2020-11-09 10:14:10 -0330

    Taylor Series Updated

---
+++ Recurrsion/taylor_series.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Taylor series
+# 1 + x/1! + x^2/2! + x^3/3! + x^4/4! + .........
+# sum(n) = 1+2+3+4+.....+n = sum(n-1) + n
+# fact(n) = 1*2*3*4*.....*n = fact(n-1) * n
+# pow(x, n) = x*n*x*.....*n = pow(x, n - 1) * x
+
+#=================================================
+#       RECURSIVE FUNCTION - TAYLOR SERIES
+#================================================= */
+
+
+int main() {
+    return 0;
+}
