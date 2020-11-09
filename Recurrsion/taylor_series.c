905c358546d74a337dcb11f65cce520b55011097
wohhie <wohhie92@gmail.com>
2020-11-09 10:27:38 -0330

    Taylor Series Updated

--- Recurrsion/taylor_series.c
+++ Recurrsion/taylor_series.c
@@ -9,14 +9,14 @@
 #=================================================
 #       RECURSIVE FUNCTION - TAYLOR SERIES
 #================================================= */
-int taylorSeries(int x, int n){
+double taylorSeries(int x, int n){
 
     static double p = 1, f = 1;
     double r;
 
 
     if(n == 0){
-        return 0;
+        return 1;
     }else{
         r = taylorSeries(x, n - 1);
         p = p * x;
