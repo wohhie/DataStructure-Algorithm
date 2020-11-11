22ed941fd9ee2ed31aaeed8234366bf52db8fbf5
wohhie <wohhie92@gmail.com>
2020-11-11 20:17:21 -0330

    update horners rules

--- Recurrsion/taylor_series_horners_rules.c
+++ Recurrsion/taylor_series_horners_rules.c
@@ -2,10 +2,16 @@
 
 
 double horners_rule(int x, int n){
-    return x/n;
+    static double result = 1;
+    if(n == 0){
+        return result;
+    }else{
+        result = 1 + (double)x/n * result;
+        return horners_rule(x, n - 1);
+    }
 }
 
 int main() {
-    printf("%lf \n", horners_rule(50, 6));
+    printf("%lf \n", horners_rule(1, 10));
     return 0;
 }
