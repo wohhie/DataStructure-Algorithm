6ee3b839d5b8d7e161e12ce019f49f39e04b4ba8
wohhie <wohhie92@gmail.com>
2020-11-14 11:21:37 -0330

    Update: Static and Dynamic Array

---
+++ Array Representation/increase_size_dynamic.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *p, *q, i;
+
+    // initialize the dynamic array
+    p = malloc(5 * sizeof(int));
+    p[0] = 3;
+    p[1] = 5;
+    p[2] = 7;
+    p[3] = 9;
+    p[4] = 11;
+
+    // increasing the size of the dynamic array
+    q = (int *) malloc(5 * sizeof(int));
+
+    // copy the data from p to q
+    for(i = 0; i < 5; i++)
+        q[i] = p[i];
+
+    free(p);
+    p = q;
+    q = NULL;
+
+
+
+
+    for(i = 0; i < 10; i++)
+        printf("%d ", p[i]);
+
+    return 0;
+}
