471637df106e89edf46171e0ea7bd632e2112be8
wohhie <wohhie92@gmail.com>
2020-11-14 13:20:07 -0330

    Dynamic Array

---
+++ Array Representation/2d_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    int A[3][4] = {
+        {1, 2, 3, 4},
+        {2, 4, 6, 8},
+        {1, 3, 5, 7}
+    };
+
+
+    // 2d dynamic array.
+    int *B[3];
+    int **C;
+    B[0] = (int *) malloc(4 * sizeof(int));
+    B[1] = (int *) malloc(4 * sizeof(int));
+    B[2] = (int *) malloc(4 * sizeof(int));
+
+
+    C = (int *) malloc(sizeof(int *))
+    C[0] = (int *) malloc(sizeof(int))
+    C[1] = (int *) malloc(sizeof(int))
+    C[2] = (int *) malloc(sizeof(int))
+
+
+
+    return 0;
+}
