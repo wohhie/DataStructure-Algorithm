cd0bc7f95f579eb5373d2fb77743d276f8d3a82f
wohhie <wohhie92@gmail.com>
2020-11-21 15:38:36 -0330

    ADT: array Merge

--- Array ADT/array_merge.c
+++ Array ADT/array_merge.c
@@ -8,8 +8,45 @@ struct Array {
 };
 
 
+// A = 1 3 5 7
+// B = 2 4 6 8
+// Result = 1 2 3 4 5 6 7 8
+
+
+
+struct Array* Merge(struct Array *arr1, struct Array *arr2){
+    int i, j, k;
+    i = j = k = 0;
+    struct Array *arr3 = (struct Array *) malloc(sizeof(struct Array));
+
+    while ( i < arr1->length && j < arr2->length){
+        if(arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else 
+            arr3->A[k++] = arr2->A[j++];
+    }
+
+    for(; i < arr1->length; i++) arr3->A[k++] = arr1->A[i++];
+    for(; j < arr1->length; j++) arr3->A[k++] = arr2->A[j++];
+
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+
+    return arr3;
+}
+
+
+
+
+
 int main() {
-    
+    struct Array arr1 = {{3, 8, 16, 20, 25}, 10, 5};
+    struct Array arr2 = {{4, 10, 12, 22, 23}, 10, 5};
+    struct Array *arr3;
+
+    *arr3 = (struct Array *) malloc((arr1.length + arr2.length) * sizeof(struct Array));
+
+
 
     return 0;
 }
