2869ac2026766bed5b8237664ceea98515671ba9
wohhie <wohhie92@gmail.com>
2020-11-29 18:38:41 -0330

    Challenge: missing number

--- Challenge/single_missing_element.c
+++ Challenge/single_missing_element.c
@@ -15,10 +15,32 @@ int missingElement(int A[], int n){
 
 }
 
+// Alternative
+// Finding single missing element from an array
+// calculating the index.
+// -------------------------------------------------
+// 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17
+// 0, 1, 2, 3,  4,  5,  6,  7,  8,  9, 10
+// ===========================================================
+void missingElementUsingIndex(int A[], int n){
+    int i, j, diff;
+    diff = A[0] - 0;
+
+    for(i = 0; i < n; i++){
+        if((A[i] - i) != diff){
+            printf("Missing Element: %d", diff + i);
+            break;
+        }
+    }
+
+}
+
+
 int main() {
     // if sorted!
-    int A[11] = {1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12};
-    printf("Missing Number: %d", missingElement(A, 11));
+    int A[11] = {6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17};
+    // printf("Missing Number: %d", missingElement(A, 11));
+    missingElementUsingIndex(A, 11);
 
     return 0;
 }
