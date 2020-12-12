8a8253f8e46f3fb96f2611939e79695369dacd70
wohhie <wohhie92@gmail.com>
2020-12-12 15:16:41 -0330

    Go primitive data

---
+++ GOLang/primitive.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// go lang binary bits
+	a := 10             // 1010
+	b := 3              // 0011
+	fmt.Println(a & b)  // 0010
+	fmt.Println(a | b)  // 1011
+	fmt.Println(a ^ b)  // 1001
+	fmt.Println(a &^ b) // 0100
+
+}
