3b0785aed734e175f7bb04d53f8db33c21f7eaa2
wohhie <wohhie92@gmail.com>
2020-12-13 10:50:02 -0330

    Challenge: Update missing element

--- Challenge/multiple_missing_element.cpp
+++ Challenge/multiple_missing_element.cpp
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct 
+void findMultipleMissingElement(int A[], int n){
+    int i, diff;
+    diff = A[0] - 0;
+
+    for(i = 0; i < n; i++){
+        if(A[i] - i != diff){
+            while(diff < A[i] - i){
+                printf("%d", diff + i);
+                printf("\n");
+                diff++;
+            }
+        }
+    }
+
+}
 
 int main() {
+    int A[11] = {6, 7, 8, 9, 11, 12, 15, 16, 17, 18, 19};
+    findMultipleMissingElement(A, 11);
 
     return 0;
 }
