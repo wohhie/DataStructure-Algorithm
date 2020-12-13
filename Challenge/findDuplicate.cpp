11b286f44c91ecedde69a5735fe8f494ba75c808
wohhie <wohhie92@gmail.com>
2020-12-13 12:16:04 -0330

    update duplicate elements

--- Challenge/findDuplicate.cpp
+++ Challenge/findDuplicate.cpp
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+// 3, 6,  8,  8, 10, 12, 15, 15, 15, 20
+// 0  1,  2,  3,  4,  5,  6,  7,  8,  9
+// i  i   i 
+//            j   j
+//            i 
+
+void findDuplicates(int A[], int n){
+    int i, j;
+    for(i = 0; i < n - 1; i++){
+        if(A[i] == A[i+1]){
+            j = i + 1; 
+            while(A[j] == A[i]) j++;
+            printf("%d appears %d times. \n", A[i], j - i);
+            i = j - 1;
+        }
+    }
+}
+
 int main() {
-    int A[11] = {3, 6, 88, 10, 12, 15, 15, 15, 20};
-    
+    int A[10] = {3, 6, 8, 8, 10, 12, 15, 15, 15, 20};
+    findDuplicates(A, 9);
     return 0;
 }
