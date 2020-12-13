e99acf8f0bccb2c9af392dbeade10ff2914f2426
wohhie <wohhie92@gmail.com>
2020-12-13 11:12:24 -0330

    update 2nd solution

--- Challenge/multiple_missing_element.cpp
+++ Challenge/multiple_missing_element.cpp
@@ -17,9 +17,26 @@ void findMultipleMissingElement(int A[], int n){
 
 }
 
+// @2nd Solution
+// ===================================================
+void nultipleMissingElement(int A[], int l, int h, int n){
+    int i;
+    int H[h] = {0};
+    for(i = 0; i < n; i++){
+        H[A[i]]++;
+    }
+
+    for(i = l; i < h; i++){
+        if(H[i] == 0) printf("%d\n", i);
+    }
+
+}
+
 int main() {
     int A[11] = {6, 7, 8, 9, 11, 12, 15, 16, 17, 18, 19};
-    findMultipleMissingElement(A, 11);
+    // findMultipleMissingElement(A, 11);
+
+    nultipleMissingElement(A, 6, 19, 11);
 
     return 0;
 }
