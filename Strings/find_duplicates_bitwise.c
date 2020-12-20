dc2d99cfae07f9538017e711df60c90487213a6f
wohhie <wohhie92@gmail.com>
2020-12-20 12:00:06 -0330

    String: find duplicates using bitwise

--- Strings/find_duplicates_bitwise.c
+++ Strings/find_duplicates_bitwise.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+void find_duplicates_bitwise(char A[]){
+
+}
+
 int main() {
+    char A[] = "finding";
+    long int H = 0, x = 0;
+    int i;
     
+    for(i = 0; A[i] != '\0'; i++){
+        x = 1;
+        x = x << (A[i] - 97);     // 5
+        
+        if((x & H) > 0){
+            printf("%c is duplicated\n", A[i]);
+        }else{
+            H = x | H; 
+        }
+    }
+
+    return 0;
 }
